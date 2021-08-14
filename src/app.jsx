9f9a6bbccceb8dbe58578d305b24b6e72b5217ad
BVBFD <lsevina126@gmail.com>
2021-08-14 19:49:40 +0900

    secrutiy of ApiKey

--- src/app.jsx
+++ src/app.jsx
@@ -12,13 +12,14 @@ function App() {
     };
     
     fetch(
-      "https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyAXV2oHIJdnkZbThPEwRNTObbaHTKYqyHA", 
+      "https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key={MyApiKey}", 
       requestOptions
       )
       .then(response => response.json())
       .then(result => setVideos(result.items))
       .catch(error => console.log('error', error));
   }, []);
+  
   return <VideoList videos={videos}/>;
 }
 
