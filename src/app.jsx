53284c9792ad37486a70159e1c20cc0b297a19a8
BVBFD <lsevina126@gmail.com>
2021-08-14 15:18:43 +0900

    show video list with title

--- src/app.jsx
+++ src/app.jsx
@@ -1,13 +1,25 @@
+import { useEffect, useState } from 'react';
 import './app.css';
+import VideoList from './components/video_list/video_list';
 
 function App() {
-  return (
-  <>
-    <h1>Hello <i class="fas fa-cat"></i></h1>
-    <h1>Hello <i class="fas fa-cat"></i></h1>
-    <h1>Hello <i class="fas fa-cat"></i></h1>
-  </>
-  );
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    const requestOptions = {
+      method: 'GET',
+      redirect: 'follow'
+    };
+    
+    fetch(
+      "https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyAXV2oHIJdnkZbThPEwRNTObbaHTKYqyHA", 
+      requestOptions
+      )
+      .then(response => response.json())
+      .then(result => setVideos(result.items))
+      .catch(error => console.log('error', error));
+  }, []);
+  return <VideoList videos={videos}/>;
 }
 
 export default App;
